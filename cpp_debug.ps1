562f15b7246e8646902d2ba93bfbc76b6cfd2523
floaq_ <3293275512@qq.com>
2025-08-20 17:01:19 +0800

    Add files via upload

---
+++ cpp_debug.ps1
@@ -0,0 +1,34 @@
+﻿# $host.UI.RawUI.ForegroundColor = "DarkGreen"
+# $host.UI.RawUI.BackgroundColor = "Black"
+
+# Invoke-Command $session -ScriptBlock { cd ..\Downloads ;Powershell ..\Downloads\SetupWeb.ps1 2> $null }
+$before_path = ($pwd).Path
+cd Desktop
+cls
+$file = "__"
+while(($file -eq "__end")-eq $false){
+    $host.UI.RawUI.ForegroundColor = "Yellow"
+    write-host "输入文件名： " -NoNewLine
+    $host.UI.RawUI.ForegroundColor = "White"
+    $file = read-host
+    $file_path = ($pwd).Path
+    if (($file -eq "__end")-or($file -eq "__cls")) {break}
+    else {clang++ "$file.cpp"}
+
+    $host.UI.RawUI.ForegroundColor = "Yellow"
+    write-host "正在调试：" -NoNewLine
+    $host.UI.RawUI.ForegroundColor = "DarkGreen"
+    write-host "$file_path\$file.cpp"
+    $host.UI.RawUI.ForegroundColor = "Green"
+    .\a
+    $host.UI.RawUI.ForegroundColor = "Yellow"
+    $host.UI.RawUI.BackgroundColor = "Black"
+    write-host "`n调试结束。"
+
+    $host.UI.RawUI.ForegroundColor = "White"
+}
+pause
+# $host.UI.RawUI.ForegroundColor = "White"
+cd $before_path
+cls
+cls
