da6f72c21d973a4b9a259fbd436b012d482eb802
floaq_ <3293275512@qq.com>
2025-08-21 22:10:53 +0800

    Add files via upload

--- reset.ps1
+++ reset.ps1
@@ -6,5 +6,9 @@ $useCnt = $configObject.Cnt
 if($isReg -eq "Yes"){$configObject.IsReg = "No"}
 if(-not($useCnt -eq 0)){$configObject.Cnt = 0}
 
+$host.UI.RawUI.ForegroundColor = "Yellow"
+write-host "数据已重置"
+$host.UI.RawUI.ForegroundColor = "Gray"
+
 $configStr = ConvertTo-Json -Depth 10 $configObject
 Set-Content -Path ".\config.json" -Value $configStr
